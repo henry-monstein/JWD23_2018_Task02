3bae069b86f9004042a4b1ea675a8e6737b2e021
Henry Monstein <1268620@gmail.com>
2018-12-22 17:35:00 +0300

    Initial commit

---
+++ src/by/epam/training/dmitrylishko/controller/Main.java
@@ -0,0 +1,8 @@
+package by.epam.training.dmitrylishko.controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/epam/training/dmitrylishko/model/entity/VacuumCleaner.java
@@ -0,0 +1,4 @@
+package by.epam.training.dmitrylishko.model.entity;
+
+public class VacuumCleaner {
+}
